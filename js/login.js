8ba72f0abd260ead54ecf866cfa488b33d815af3
maxipz <maximilianopz96@gmail.com>
2023-08-15 19:49:07 -0300

    Desafiate Entrega 1
    Desafio

--- js/login.js
+++ js/login.js
@@ -1,4 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
+//document.addEventListener("DOMContentLoaded", function() {
+  //  const form = document.querySelector("form");
+  //
+    //form.addEventListener("submit", function(event) {
+      //event.preventDefault();
+  
+      //const username = document.getElementById("username").value;
+      //const password = document.getElementById("password").value;
+  
+      //if (username.trim() === "" || password.trim() === "") {
+        //alert("Por favor, completa todos los campos.");
+      //} else {
+       // window.location.href = "index.html"; 
+      //}
+    //});
+  //});
+  document.addEventListener("DOMContentLoaded", function() {
     const form = document.querySelector("form");
   
     form.addEventListener("submit", function(event) {
@@ -10,7 +26,17 @@ document.addEventListener("DOMContentLoaded", function() {
       if (username.trim() === "" || password.trim() === "") {
         alert("Por favor, completa todos los campos.");
       } else {
-        window.location.href = "index.html"; 
+        // Simulación de autenticación exitosa
+        const autenticacionExitosa = true;
+  
+        if (autenticacionExitosa) {
+          localStorage.setItem("username", username);
+          localStorage.setItem("isLoggedIn", true);
+  
+          window.location.href = "index.html"; // Redirige a la página principal después del inicio de sesión
+        } else {
+          alert("Credenciales incorrectas. Por favor, intenta de nuevo.");
+        }
       }
     });
   });
