6a66a87aaa5f5bb51c90556f353120d32126f56a
maxipz <maximilianopz96@gmail.com>
2023-08-19 17:46:17 -0300

    Desafiate 1.2
    Arreglue el sessionStorage

--- js/login.js
+++ js/login.js
@@ -1,44 +1,20 @@
-//document.addEventListener("DOMContentLoaded", function() {
-  //  const form = document.querySelector("form");
-  //
-    //form.addEventListener("submit", function(event) {
-      //event.preventDefault();
-  
-      //const username = document.getElementById("username").value;
-      //const password = document.getElementById("password").value;
-  
-      //if (username.trim() === "" || password.trim() === "") {
-        //alert("Por favor, completa todos los campos.");
-      //} else {
-       // window.location.href = "index.html"; 
-      //}
-    //});
-  //});
-  
-  document.addEventListener("DOMContentLoaded", function() {
-    const form = document.querySelector("form");
-  
-    form.addEventListener("submit", function(event) {
-      event.preventDefault();
-  
-      const username = document.getElementById("username").value;
-      const password = document.getElementById("password").value;
-  
-      if (username.trim() === "" || password.trim() === "") {
-        alert("Por favor, completa todos los campos.");
-      } else {
-        // Simulación de autenticación exitosa
-        const autenticacionExitosa = true;
-  
-        if (autenticacionExitosa) {
-          sessionStorage.setItem("username", username);
-          sessionStorage.setItem("isLoggedIn", true);
-  
-          window.location.href = "index.html"; // Redirige a la página principal después del inicio de sesión
-        } else {
-          alert("Credenciales incorrectas. Por favor, intenta de nuevo.");
-        }
-      }
-    });
+document.addEventListener("DOMContentLoaded", function() {
+  const form = document.querySelector("form");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if (username.trim() === "" || password.trim() === "") {
+      alert("Por favor, completa todos los campos.");
+    } else {
+      sessionStorage.setItem("username", username);
+      sessionStorage.setItem("isLoggedIn", true);
+
+      window.location.href = "index.html"; 
+    }
   });
+});
   
