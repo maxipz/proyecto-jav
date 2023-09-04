941e26768c435fac123d40f8359d195984f100fd
maxipz <maximilianopz96@gmail.com>
2023-09-04 19:28:49 -0300

    Entrega 3 Pauta 1 y 2 incompleta
    Pauta 1 completa y la pauta 2 faltan imagenes.

---
+++ js/product-info.js
@@ -0,0 +1,24 @@
+function ObtenerInfo(ID) {
+    const url = `https://japceibal.github.io/emercado-api/products/${ID}.json`;
+    fetch(url)
+      .then(response => response.json())
+      .then(resultado => {
+       const Precio = document.getElementById("Precio");
+       Precio.innerHTML = "Precio " + resultado.cost;
+
+       const Descripcion = document.getElementById("Descripcion");
+       Descripcion.innerHTML = "Descripcion " + resultado.description;
+
+       const Categoria = document.getElementById("Categorias");
+       Categoria.innerHTML = "Categorías " + resultado.category;
+
+       const CantidadVendidos = document.getElementById("CantVendidos");
+       CantidadVendidos.innerHTML = "Cantidad de Vendidos " + resultado.soldCount;
+      })
+      .catch(error => console.error('Ocurrió un error:', error)); 
+  }
+  
+  
+  window.onload = function() {
+    ObtenerInfo(localStorage.getItem("IdProducto"));
+  };
